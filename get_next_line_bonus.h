7e6c67059816f0dbf50782ca9e947a4b0cada417
vgroux <vgroux@student.42lausanne.ch>
2022-10-28 14:15:54 +0200

    bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 17:49:01 by vgroux            #+#    #+#             */
+/*   Updated: 2022/10/28 14:08:22 by vgroux           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_gnl_read(int fd, char *buffer);
+char	*ft_gnl_get_line(char *buffer);
+char	*ft_gnl_remove_old_line(char *buffer);
+void	*ft_calloc(size_t nb_elem, size_t size_elem);
+int		ft_gnl_strchr(char *str);
+char	*ft_gnl_strjoin(char *line, char *buff);
+size_t	ft_strlen(char *str);
+char	*ft_strndup(char *str, size_t end);
+char	*ft_gnl_remove_free(char *buffer);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 17:51:12 by vgroux            #+#    #+#             */
+/*   Updated: 2022/10/28 14:04:55 by vgroux           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_calloc(size_t nb_elem, size_t size_elem)
+{
+	void			*ptr;
+	size_t			i;
+	unsigned char	c;
+
+	ptr = malloc(nb_elem * size_elem);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	c = '\0';
+	while (i < (nb_elem * size_elem))
+	{
+		((char *)ptr)[i] = c;
+		i++;
+	}
+	return (ptr);
+}
+
+int	ft_gnl_strchr(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	*ft_gnl_strjoin(char *line, char *buff)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+	size_t	tlen;
+
+	tlen = ft_strlen(line) + ft_strlen(buff) + 1;
+	str = (char *)ft_calloc(tlen, sizeof(char));
+	if (!str)
+		free(line);
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (line[i])
+	{
+		str[i] = line[i];
+		i++;
+	}
+	while (buff[j])
+	{
+		str[i++] = buff[j++];
+	}
+	free(line);
+	str[i] = '\0';
+	return (str);
+}
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strndup(char *buffer, size_t end)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)ft_calloc(end + 2, sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i <= end && buffer[i])
+	{
+		str[i] = buffer[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
